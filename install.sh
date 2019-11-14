444d5bcc8eb906a9aa506b9630523cf7ac50827c
du2016 <leave24534@gmail.com>
2019-11-14 18:20:21 +0800

    add install mosquitto

--- install.sh
+++ install.sh
@@ -18,8 +18,10 @@ sed -i "s/MASTER_NODE/$MASTER_NODE/g" edgecore/conf/edge.yaml
 scp ./install-docker.sh $EDGE_NODE:/tmp/
 scp -r kubeedge-certs $EDGE_NODE:/etc/kubeedge
 scp -r edgecore $EDGE_NODE:/usr/local/
+
 ssh $EDGE_NODE "bash /tmp/install-docker.sh && rm -f /tmp/install-docker.sh"
 ssh $EDGE_NODE 'cd /usr/local/edgecore;chmod +x edgecore;nohup ./edgecore &>> /var/log/edgecore.log &'
+ssh $EDGE_NODE 'yum install epel* -y && yum install -y mosquitto && mosquitto -d -p 1883'
 
 
 
